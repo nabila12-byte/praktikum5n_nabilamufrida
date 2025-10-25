f68333ba605d2a31389cf4e8cee841b8455c5698
unknown <nabilamufrida@gmail.com>
2025-10-25 15:28:52 +0800

    Komit pertama

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function admin()
+    {
+        return view('admin.beranda');
+    }
+}
